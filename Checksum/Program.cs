4146d9b72ccd4db665294b1c1b5e56277e0b70a2
thomasdc <1496708+thomasdc@users.noreply.github.com>
2021-07-19 17:24:17 +0200

    checksum part 1

--- Checksum/Program.cs
+++ Checksum/Program.cs
@@ -1,3 +1,35 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using Shouldly;
+using Xunit;
 
-Console.WriteLine("Hello World!");
+Console.WriteLine(File.ReadAllText("input.txt").CalculateChecksum());
+
+public static class Solver
+{
+    public static double CalculateChecksum(this string inputString)
+    {
+        var numbers = inputString.Select(_ => int.Parse(_.ToString())).ToArray();
+        double checksum = 0;
+        for (var i=0; i<numbers.Length; i++)
+        {
+            checksum += numbers[i] == numbers[(i + 1) % numbers.Length] ? numbers[i] : 0;
+        }
+        
+        return checksum;
+    }
+}
+
+public static class Tests
+{
+    [Theory]
+    [InlineData("1122", 3)]
+    [InlineData("1111", 4)]
+    [InlineData("1234", 0)]
+    [InlineData("91212129", 9)]
+    public static void ValidateChecksumExamples(string input, double expectedSum) => input.CalculateChecksum().ShouldBe(expectedSum);
+
+    [Fact]
+    public static void ValidateInputExample() => File.ReadAllText("input.txt").CalculateChecksum().ShouldBe(1171);
+}
